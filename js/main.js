03f2ffe913f1a6470c139124183abd1f01280128
Kribas Rimal <32868850+Kribas@users.noreply.github.com>
2022-03-06 17:37:30 +0545

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -17,11 +17,14 @@ $(window).on("scroll", function() {
 		$(".header").addClass("header-sticky");
         $(".header__top").addClass("header__top-sticky");
         $(".navbar").addClass("navbar-sticky");
+		$(".logo").addClass("logo-sticky");
+		$(".social-icons").addClass("social-icons-sticky");
 
 	} else {
 	   $(".header").removeClass("header-sticky");
        $(".header__top").removeClass("header__top-sticky");
        $(".navbar").removeClass("navbar-sticky");
-
+	   $(".logo").removeClass("logo-sticky");
+	   $(".social-icons").removeClass("social-icons-sticky");
 	}
 });
